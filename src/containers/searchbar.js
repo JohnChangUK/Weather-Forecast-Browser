68a37af8f21f765ea10fb406fd40379c1ee5e6e0
John Chang <johnchang9094@gmail.com>
2017-03-30 23:31:32 +0100

    Controlled Components and Binding This Keyword

--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { term: '' };
+                        // this, is our instance of SearchBar, 
+                        // has a function called onInputChange
+                        // bind that function to this, which is SearchBar
+                        // and replace onInputChange with this new bound instance of this function
+                        // Overring the LHS (left hand side value)
+     this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
         <form className="input-group">
-          <input />
+          <input 
+            placeholder="Get a five day forecast in your city"
+            className="form-control"
+            value={this.state.term}
+            onChange={this.onInputChange} />
           <span className="input-group-btn">
             <button type="submit" className="btn btn-secondary">Submit</button>
           </span>
