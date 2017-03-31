6f36b6c349774f0d512f4605a195c19dadf001c0
John Chang <johnchang9094@gmail.com>
2017-03-31 22:31:49 +0100

    Added AJAX requests with Axios and Promises

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,20 @@
+import axios from 'axios';
+
 const API_KEY = 'e4f4eb126438f8cf7d9803f8ed0e18db';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},uk`;
+  // request will return a promise
+  const request = axios.get(url);
 
   return {
-    type: FETCH_WEATHER
-  }
+    type: FETCH_WEATHER,
+    // payload is an optional property that goes along with actions
+    // that can contain some additional data that describes
+    // this particular action
+    payload: request
+  };
 }
