6fb0e77acc24519ecb87e7eba56023675962a6d1
John Chang <johnchang9094@gmail.com>
2017-04-01 13:00:52 +0100

    Built List Container, imported connect function, defined function mapStateToProps(state), and pull the weather data into our container

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,10 +4,10 @@ export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER: 
       // return state.concat([action.payload.data]);
-      return [action.payload.data , ...state ]; //This returns [city, city, city]
+      return [ action.payload.data, ...state ]; //This returns [city, city, city]
                                               //NOT [city, [city, city] ]
       // ... is the Spread Operator, spreads the array in its individual components
-      
+
   }
   return state;
 }
